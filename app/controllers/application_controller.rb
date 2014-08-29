af9cd9b0fd375e3e73379a671782022de6439478
Ahmed ElBayaa <ahmed_2765@yahoo.com>
2014-08-30 01:00:16 +0200

    permit user's attributes in update_account

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,6 +9,7 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.for(:sign_up) << [:usernamem, :name]
+    devise_parameter_sanitizer.for(:sign_up) << [:usernamem, :name, :language_id]
+    devise_parameter_sanitizer.for(:account_update) << [:usernamem, :name, :language_id]
   end
 end
